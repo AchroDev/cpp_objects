49a980cda087f633b8759ee22b507c61a9536903
AchroDev <achrodev@proton.me>
2024-09-28 14:08:27 -0500

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+#include <string>
+
+// Using this to simplify declaring/calling strings
+using String = std::string;
+
+// Objects and how to properly create/instantiate them in C++
+
+// Example Entity class
+class Entity
+{
+    // Single private object 'm_Name'
+private:
+    String m_Name;
+    // Public section
+public:
+    // Default constructor initializing m_Name
+    Entity() : m_Name("Unkown") {}
+    // Other constructor setting m_Name to the referenced 'name' value
+    Entity(const String &name) : m_Name(name) {}
+
+    // Simple getter for the name
+    const String &GetName() const { return m_Name; }
+};
 
 int main()
 {
